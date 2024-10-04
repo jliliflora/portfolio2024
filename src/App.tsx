41401224ac6a2bf3d0cc45d3f0d888e9c0ffd12a
jliliflora <syon704@gmail.com>
2024-10-04 20:43:12 +0900

    main animation 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { motion } from "framer-motion";
 import React from "react";
+import styled from "styled-components";
+import SectionBg from "./components/SectionBg";
+import OrbitAni from "./components/OrbitAni";
+
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+`;
 
 function App() {
-  return <div>test</div>;
+  return (
+    <Wrapper>
+      <SectionBg />
+      <OrbitAni />
+    </Wrapper>
+  );
 }
 
 export default App;
