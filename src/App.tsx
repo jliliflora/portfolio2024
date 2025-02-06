8fbff449f74b19b052e1f5f1aa283121c6c04470
jliliflora <syon704@gmail.com>
2025-02-06 19:36:33 +0900

    react-helmet 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Main from "./Main";
 import Experience from "./Experience";
+import { Helmet } from "react-helmet";
 
 const MainWrapper = styled.div`
   overflow: hidden;
@@ -9,6 +10,9 @@ const MainWrapper = styled.div`
 function App() {
   return (
     <>
+      <Helmet>
+        <title>2025 SY's PortFolio</title>
+      </Helmet>
       <MainWrapper>
         <Main />
       </MainWrapper>
